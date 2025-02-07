c9f601429a5920bef4531e8859e4e2abc0771b53
hejohns <hejohns@umich.edu>
2025-02-07 08:13:02 -0500

    init
    tested ftxui examples to see if they compiled and ran

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//import "print"
+#include <print>
+#include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/screen/screen.hpp>
+
+int main(){
+    std::println("hello world");
+    return 0;
+}
